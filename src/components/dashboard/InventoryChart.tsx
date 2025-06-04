64f19e6b158bc6f96501c7461d7c746e295cf1f5
or23 <avtipoos@gmail.com>
2025-06-04 22:39:06 +0300

    Updated InventoryChart.tsx

--- src/components/dashboard/InventoryChart.tsx
+++ src/components/dashboard/InventoryChart.tsx
@@ -22,7 +22,7 @@ export function InventoryChart({ data, title, loading = false }: InventoryChartP
           <div className="h-[300px] bg-gray-800 animate-pulse rounded" />
         ) : (
           <div className="h-[300px]">
-            <ResponsiveContainer width="100%" height="100%">
+            <ResponsiveContainer width="100%\" height="100%">
               <BarChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 20 }}>
                 <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#333" />
                 <XAxis dataKey="name" axisLine={false} tickLine={false} tick={{ fill: '#aaa' }} />
