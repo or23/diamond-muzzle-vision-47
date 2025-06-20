67311be899283059720b72c2712e4f218bd8ed3d
or23 <avtipoos@gmail.com>
2025-06-20 15:57:52 +0300

    Updated StatCard.tsx

--- src/components/dashboard/StatCard.tsx
+++ src/components/dashboard/StatCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { LucideIcon } from "lucide-react";
+import { DivideIcon as LucideIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 interface StatCardProps {
